88bbf83f1d4d4da4428f391ae83a1743c26a40c0
xiaozhuweixz <cxw@boozinfo.com>
2018-10-18 00:00:26 +0800

    tornado项目前后台测试登录系统

--- src/tornadoFile.py
+++ src/tornadoFile.py
@@ -3,29 +3,37 @@ import tornado.web
 import  os
 
 
-INPUT_LIST = []
+INPUT_LIST = {'longInFlage': None}
 
 class MainHandler (tornado.web.RequestHandler):
     def get(self):
+        INPUT_LIST['longInFlage'] = None
+        self.render(os.path.join('index.html'), user_info=INPUT_LIST)
 
-        #self.write("Hello, world")
-
-        self.render(os.path.join('s2.html'), xxx=INPUT_LIST)
+class LonginHandler (tornado.web.RequestHandler):
     def post(self, *args, **kwargs):
-        data = self.get_argument("xxx")
-        INPUT_LIST.append(data)
-        self.render(os.path.join('s2.html'), xxx=INPUT_LIST)
+        name = self.get_argument("name")
+        pwd = self.get_argument("pwd")
+        if name == "cxw" and pwd == "123":
+            INPUT_LIST['longInFlage'] = True
+            INPUT_LIST['name'] = name
+            self.render(os.path.join("index.html"), user_info=INPUT_LIST)
+
+        self.redirect("/index")
 
 settings = {
-    'template_path': 'template/test'
+    'template_path': 'template',
+    'static_path': 'static',
+    'static_url_prefix': '/static/'
 }
-
 
 #路由映射
 application = tornado.web.Application ([
     ("/index", MainHandler),
+    ("/longIn", LonginHandler)
 ], **settings)
 
 if __name__ == "__main__":
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
+
