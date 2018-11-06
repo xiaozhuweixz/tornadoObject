34ebe47b717e8c530c0f3bbc723a41eed72eeb9e
caixiaowei <cxw@boozinfo@.com>
2018-11-06 15:40:47 +0800

    ajax

---
+++ src/jsonPobject.py
@@ -0,0 +1,28 @@
+import tornado.ioloop
+import tornado.web
+import  os
+
+class IndexHandler (tornado.web.RequestHandler):
+    def get(self):
+        self.render("ajax/JqueryAjaxHtml.html")
+
+    def post(self):
+        print(self.get_argument("k1"))
+        print(self.get_argument("k2"))
+        self.write("jsonP,OK")
+
+settings = {
+    'template_path': 'template',
+    'static_path': 'static',
+    'static_url_prefix': '/static/'
+}
+
+#路由映射
+application = tornado.web.Application ([
+    ("/index", IndexHandler)
+], **settings)
+
+if __name__ == "__main__":
+    application.listen(8881)
+    tornado.ioloop.IOLoop.instance().start()
+
